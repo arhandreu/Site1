eeda89a3b4849092947d3ec03c9f7cb96f98026f
Andreu Bardukov <arhandreu@gmail.com>
2022-02-16 13:54:51 +0500

    2

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('category/<slug:slug>/', PostsByCategory.as_view(), name='category'),
     path('tag/<slug:slug>/', PostsByTag.as_view(), name='tag'),
-    path('post/<slug:slug>/', GetPost.as_view(), name='post')
+    path('post/<slug:slug>/', GetPost.as_view(), name='post'),
+    path('search', Search.as_view(), name='search'),
 ]
